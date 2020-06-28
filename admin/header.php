537728ba4689767a75fe4c57044403089a233ac3
dhaniaffa <dhaniaffa@gmail.com>
2020-06-28 08:01:56 +0700

    finalisasi

--- admin/header.php
+++ admin/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
     <script type="text/javascript" src="../assets/js/jquery-3.5.1.js"></script>
     <script type="text/javascript" src="../assets/js/bootstrap.js"></script>
-    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    <script src="https://kit.fontawesome.com/50b8e9bc0f.js" crossorigin="anonymous"></script>
 
 </head>
 <body style="background: #f0f0f0">
@@ -26,30 +26,30 @@ if ($_SESSION['status']!="status"){
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="nav navbar-nav collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php"><i class="glyphicon glyphicon-home"></i>Beranda</a>
+                <a class="nav-link" href="index.php"><i class="fas fa-house-user mr-1"></i>Beranda</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="pelanggan.php">Pelanggan</a>
+                <a class="nav-link" href="pelanggan.php"><i class="fas fa-male mr-1"></i>Pelanggan</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="transaksi.php">Transaksi</a>
+                <a class="nav-link" href="transaksi.php"><i class="fas fa-comment-dollar mr-1"></i>Transaksi</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="laporan.php">Laporan</a>
+                <a class="nav-link" href="laporan.php"><i class="fas fa-file-alt mr-1"></i>Laporan</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-cog mr-1"></i>
                     Pengaturan
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="harga.php">Pengaturan Harga</a>
+                    <a class="dropdown-item" href="harga.php"><i class="fas fa-dollar-sign mr-1"></i>Pengaturan Harga</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="GantiPassword.php">Ganti Password</a>
+                    <a class="dropdown-item" href="GantiPassword.php"><i class="fas fa-unlock-alt mr-1"></i>Ganti Password</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="setting/logout.php">Keluar</a>
+                    <a class="dropdown-item" href="setting/logout.php"><i class="fas fa-sign-out-alt mr-1"></i>Keluar</a>
                 </div>
             </li>
         </ul>
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,7 @@
-<?php include 'header.php';?>
+<?php
+include 'header.php';
+include "../koneksi.php";
+?>
 <div class="container">
     <div class="alert alert-info text-center">
         <h4 style="margin-bottom: 0px"><b>Selamat Datang Di Laundry Dhaniaffa</b></h4>
@@ -9,7 +12,129 @@
             <h4>Dashboard</h4>
         </div>
         <div class="card-body">
-            <h4>Halo</h4>
+            <div class="row">
+                <div class="col-md-3" onclick="location.href='pelanggan.php';" style="cursor: pointer;">
+                    <div class="card bg-primary">
+                        <div class="card-header text-white">
+                            <h1>
+                                <i class="fas fa-user text-white"></i>
+                                <span class="float-lg-right text-white">
+                                <?php
+                                $pelanggan = mysqli_query($koneksi, "select * from pelanggan");
+                                echo mysqli_num_rows($pelanggan);
+                                ?>
+                            </span>
+                            </h1>
+                            <br>
+                            <b>Jumlah Pelanggan</b>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3" onclick="location.href='transaksi.php';" style="cursor: pointer;">
+                    <div class="card bg-warning">
+                        <div class="card-header">
+                            <h1>
+                                <i class="fas fa-spinner"></i>
+                                <span class="float-lg-right">
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='0'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            <br>
+                            <b>Cucian Yang Diproses</b>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3" onclick="location.href='transaksi.php';" style="cursor: pointer;">
+                    <div class="card" style="background: lightskyblue">
+                        <div class="card-header">
+                            <h1>
+                                <i class="fas fa-tshirt"></i>
+                                <span class="float-lg-right">
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='1'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            <br>
+                            <b>Cucian Yang Sedang Dicuci</b>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3" onclick="location.href='transaksi.php';" style="cursor: pointer;">
+                    <div class="card" style="background: lightgreen">
+                        <div class="card-header">
+                            <h1>
+                                <i class="far fa-check-circle"></i>
+                                <span class="float-lg-right">
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='2'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            <br>
+                            <b>Cucian Yang Sudah Selesai</b>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="card">
+        <div class="card-header">
+            <h4>Riwayat Transaksi Terkini</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <td width="1%">No</td>
+                    <td>Invoice</td>
+                    <td>Tanggal</td>
+                    <td>Pelanggan</td>
+                    <td>Berat</td>
+                    <td>Tgl. Selesai</td>
+                    <td>Harga</td>
+                    <td>Status</td>
+                </tr>
+                <?php
+                $data= mysqli_query($koneksi, "select * from transaksi,pelanggan where transaksi_pelanggan=pelanggan_id order by transaksi_id desc  limit 5");
+                $hasil = mysqli_fetch_all($data, MYSQLI_ASSOC);
+                $count = 1;
+                foreach ($hasil as $item){
+                ?>
+                <tr>
+                    <td><?php echo $count;?></td>
+                    <td><?php echo "INVOICE-".$item['transaksi_id'];?></td>
+                    <td><?php echo $item['transaksi_tgl'];?></td>
+                    <td><?php echo $item['pelanggan_nama'];?></td>
+                    <td><?php echo $item['transaksi_berat'];?></td>
+                    <td><?php echo $item['transaksi_tgl_selesai'];?></td>
+                    <td><?php echo $item['transaksi_harga'];?></td>
+                    <td><?php
+                        if ($item['transaksi_status']=="0"){
+                            echo "Sedang Diproses";
+                        }elseif ($item['transaksi_status']=="1"){
+                            echo "Sedang Dicuci";
+                        }elseif ($item['transaksi_status']=="2"){
+                            echo "Selesai";
+                        }
+                        ?></td>
+                </tr>
+                <?php
+                 $count++;
+                }
+                 ?>
+            </table>
         </div>
     </div>
 </div>
